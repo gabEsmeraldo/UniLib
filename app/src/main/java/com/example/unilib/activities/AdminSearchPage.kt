c026d0163f002650aedd4c45dcbcc0f9b55905d7
Cauettt <cauetrodrigues@gmail.com>
2026-04-24 18:47:37 -0300

    Criação das activities e entrega das transições de tela das minhas telas
    O padrão de nomenclatura deve ser CamelCase então algumas activities ainda estão com nome errado, tirando isso acredito que minhas transições estão ok, mas falta a implementação das transições de home page para teste

---
+++ app/src/main/java/com/example/unilib/activities/AdminSearchPage.kt
@@ -0,0 +1,42 @@
+package com.example.unilib.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import com.example.unilib.R
+
+class AdminSearchPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.admin_search_page)
+
+        val cardAlgoritmos = findViewById<CardView>(R.id.CardView1)
+        val cardEngenharia = findViewById<CardView>(R.id.CardView2)
+        val cardCleanCode = findViewById<CardView>(R.id.CardView3)
+        val cardRedes = findViewById<CardView>(R.id.CardView4)
+
+        cardAlgoritmos.setOnClickListener {
+            irParaDetalhes("Algoritmos e Estruturas de Dados")
+        }
+
+        cardEngenharia.setOnClickListener {
+            irParaDetalhes("Engenharia de Software")
+        }
+
+        cardCleanCode.setOnClickListener {
+            irParaDetalhes("Clean Code")
+        }
+
+        cardRedes.setOnClickListener {
+            irParaDetalhes("Redes de Computadores")
+        }
+
+    }
+
+    private fun irParaDetalhes(titulo: String) {
+        val intent = Intent(this, BookDetails::class.java)
+        intent.putExtra("TITULO_LIVRO", titulo)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/unilib/activities/admin_book_details.kt
@@ -0,0 +1,14 @@
+package com.example.unilib.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.unilib.R
+
+class admin_book_details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
