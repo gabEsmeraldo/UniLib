ccf3aed6b5b812b490bfbeea72b2de74c5a980c8
Matheus Teixeira <matheeusteixeeira@gmail.com>
2026-05-04 16:22:13 -0300

    Navegação da tela de cadastro

--- app/src/main/java/com/example/unilib/activities/CadastroPage.kt
+++ app/src/main/java/com/example/unilib/activities/CadastroPage.kt
@@ -1,10 +1,36 @@
 package com.example.unilib.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class CadastroPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.cadastro_page)
+
+        val btnBack = findViewById<ImageButton>(R.id.btnBack)
+        val btnCadastrar = findViewById<Button>(R.id.btnCadastrar)
+        val btnFazerLogin = findViewById<Button>(R.id.btnFazerLogin)
+
+        btnBack.setOnClickListener {
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnCadastrar.setOnClickListener {
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnFazerLogin.setOnClickListener {
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
