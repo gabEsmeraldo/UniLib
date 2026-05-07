edd6381d2ee0633a4ea8122ceb3e7c3ff1f149b8
Janylson Filho <janylsonfilho34@hotmail.com>
2026-05-07 12:07:58 -0300

    Add Modal Devolucao Atrasada

--- app/src/main/java/com/example/unilib/activities/emprestimo_page.kt
+++ app/src/main/java/com/example/unilib/activities/emprestimo_page.kt
@@ -1,6 +1,7 @@
 package com.example.unilib.activities
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.unilib.R
 
@@ -10,5 +11,18 @@ class emprestimo_page : AppCompatActivity() {
         setContentView(R.layout.emprestimos_page)
 
         NavBarHelper.setup(this, NavTab.LOANS)
+
+        setupDevolucaoAtrasadaModal()
+    }
+
+    private fun setupDevolucaoAtrasadaModal() {
+        findViewById<View>(R.id.cardEmprestimoAtrasado)?.setOnClickListener {
+            DevolucaoAtrasadaModalHelper.show(
+                activity = this,
+                nomeLivro = "Design Patterns",
+                diasAtraso = 5,
+                taxa = "R$2,50"
+            )
+        }
     }
 }
